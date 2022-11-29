1c0d66feceec2f88e3d32ceb5efc6e564ff6f59d
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 <wngns0812@gmail.com>
2022-11-29 12:45:26 +0900

    Revert "add join function"
    This reverts commit 3133ff0ec4e21b27f7f79444844a916cf3a72d4f.

--- Theater_Project/src/user/userDAO.java
+++ Theater_Project/src/user/userDAO.java
@@ -45,49 +45,4 @@ public class userDAO {
 		}
 	return -2;
     }
-    
-        public int joinUser(userDTO user) throws SQLException {
-		String sql = "insert into user(userId, userPw) values(?,?)";
-		pstmt = conn.prepareStatement(sql);
-		try {
-			pstmt.setString(1, user.getId());
-			pstmt.setString(2, user.getPw());
-			return pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
-	}
-        public int joinUserInfo(userDTO user) throws SQLException {
-		String sql = "insert into userInfo(userId, userName, userEmail, userGender) values(?,?,?,?)";
-		pstmt = conn.prepareStatement(sql);
-		try {
-			pstmt.setString(1, user.getId());
-			pstmt.setString(2, user.getName());
-			pstmt.setString(3, user.getGender());
-			pstmt.setString(4, user.getEmail());
-			return pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
-	}
-	
-	public int joinCheck(String userID) throws SQLException {
-		String sql = "select * from user where userID = ?";
-		pstmt = conn.prepareStatement(sql);
-		try {
-			ResultSet rs = null;
-			pstmt.setString(1, userID);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				return 1;
-			} else {
-				return 0;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
-	}
 }
